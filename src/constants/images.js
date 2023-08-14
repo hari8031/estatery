f889419da1c45ed8ef0faeac97bb83d81b03f71d
hari <thisismekrishnan@gmail.com>
2023-08-15 00:09:25 +0530

    first commit

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,25 @@
 import logo from "../assets/logo.svg";
+import asd1 from "../assets/asd1.jpeg";
+import asd2 from "../assets/asd2.jpeg";
+import asd3 from "../assets/asd3.jpeg";
+import asd4 from "../assets/asd4.jpeg";
+import asd5 from "../assets/asd5.jpeg";
+import asd6 from "../assets/asd6.jpeg";
+import asd7 from "../assets/asd7.jpeg";
+import asd8 from "../assets/asd8.jpeg";
+import asd9 from "../assets/asd9.jpeg";
+import asd10 from "../assets/asd10.jpeg";
 
-export default {logo}
+export default {
+  logo,
+  asd1,
+  asd2,
+  asd10,
+  asd3,
+  asd4,
+  asd5,
+  asd6,
+  asd7,
+  asd8,
+  asd9,
+};
